2944727d467340afec755e2ad4aad1c057b1c68d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-03-27 11:56:04 +0200

    Add format on save

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 vim.opt.colorcolumn = "80"
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- format with conform on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
