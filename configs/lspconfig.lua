248cc0fb06b3b1f2d20edd38baec84edd0182bff
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-03-27 11:54:50 +0200

    Format

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,12 +3,12 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-lspconfig.pylsp.setup({
+lspconfig.pylsp.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
 
-lspconfig.tsserver.setup({
+lspconfig.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-})
+}
